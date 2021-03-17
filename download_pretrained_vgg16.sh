250d766f6b3b092e975c40a36069eab5d9c715b1
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-03-17 23:16:56 +0100

    Update script for downloading pre-trained VGG16-Net

--- download_pretrained_vgg16.sh
+++ download_pretrained_vgg16.sh
@@ -1,6 +1,5 @@
-git clone https://github.com/vadimkantorov/caffemodel2pytorch
+git clone https://github.com/vadimkantorov/caffemodel2pytorch.git
 cd caffemodel2pytorch
-wget https://github.com/CSAILVision/places365/blob/master/deploy_vgg16_places365.prototxt
 wget http://places2.csail.mit.edu/models_places365/vgg16_places365.caffemodel
 python -m caffemodel2pytorch vgg16_places365.caffemodel -o '../vgg_places365.h5'
 cd ../
