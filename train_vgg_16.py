eaf1de7f6becd12311a82f17163351a4ff3e849f
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-03-21 18:56:28 +0100

    Adjust default learning rate

--- train_vgg_16.py
+++ train_vgg_16.py
@@ -39,7 +39,7 @@ parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
 parser.add_argument('-b', '--batch-size', default=256, type=int,
                     metavar='N', help='mini-batch size (default: 256)')
-parser.add_argument('--lr', '--learning-rate', default=0.01, type=float,
+parser.add_argument('--lr', '--learning-rate', default=0.001, type=float,
                     metavar='LR', help='initial learning rate')
 parser.add_argument('--print-freq', '-p', default=10, type=int,
                     metavar='N', help='print frequency (default: 10)')
