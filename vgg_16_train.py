6adc6426a8d6b5434ad573c3a61254ff16a7ec17
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-03-22 02:42:06 +0100

    Update hyperparameters

--- vgg_16_train.py
+++ vgg_16_train.py
@@ -30,7 +30,7 @@ parser.add_argument('--data', metavar='DIR',
                     help='path to dataset')
 parser.add_argument('-j', '--workers', default=64, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
-parser.add_argument('--epochs', default=90, type=int, metavar='N',
+parser.add_argument('--epochs', default=3, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
